bc047e03972fb8cff662004cc7ca8fdce8ece15a
sokaribo <cotterellsoks@gmail.com>
2023-03-08 01:47:20 -0800

    0x01.Python-if/else,loops,functions

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for digit in range(0, 100):
-    if digit == 99:
-        print("{}".format(digit))
-    else:
-        print("{:02}".format(digit), end=', ')
+for num in range(0, 99):
+    print('{0} = {1}'.format(num, hex(num)))
